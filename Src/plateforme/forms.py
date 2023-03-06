8965c1e81b8776cc5cee50b5b2a1c5b8c66bce91
ali segueg <alisegueg123@outllok.fr>
2023-03-06 17:55:06 +0100

    front web

--- Src/plateforme/forms.py
+++ Src/plateforme/forms.py
@@ -17,13 +17,7 @@ class InterimUserForm(UserCreationForm):
 from .models import EmpUser
 
 class EmpUserForm(UserCreationForm):
-    """
-    societe = forms.CharField(max_length=30, required=True)
-    nom = forms.CharField(max_length=30, required=True)
-    adresse = forms.CharField(max_length=200, required=True)
-    telephone = forms.CharField(max_length=20, required=True)
-    """
 
     class Meta:
         model = EmpUser
-        fields = ['username', 'email', 'password1', 'password2', 'societe', 'nom', 'adresse', 'telephone']
+        fields = ['username', 'email', 'password1', 'password2', 'societe', 'nom', 'adresse', 'telephone', 'domaine']
--- Src/plateforme/models.py
+++ Src/plateforme/models.py
@@ -19,5 +19,6 @@ class EmpUser(Utilisateur):
     nom = models.CharField(max_length=30, blank=True)
     adresse = models.CharField(max_length=200, blank=True)
     telephone = models.CharField(max_length=20, blank=True)
+    domaine = models.CharField(max_length=20, blank=True)
     def __str__(self):
         return self.EmpUser.societe
