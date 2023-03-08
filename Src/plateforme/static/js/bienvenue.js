7c8e93801e448c648c95f9b1ddcb6ce12c6cdf14
ali segueg <alisegueg123@outllok.fr>
2023-03-08 16:31:09 +0100

    commence le front avec js

--- Src/plateforme/static/js/bienvenue.js
+++ Src/plateforme/static/js/bienvenue.js
@@ -2,21 +2,22 @@
 /* les different type d'inscription */
 
 document.addEventListener("DOMContentLoaded", function() {
-    var interimaireOption = document.getElementById("interimaire-option");
-    var employeurOption = document.getElementById("employeur-option");
-        
-    interimaireOption.style.display = "none";
-    employeurOption.style.display = "none";
-  
+  var interimaireOption = document.getElementById("interimaire-option");
+  var employeurOption = document.getElementById("employeur-option");
+      
+  interimaireOption.style.display = "none";
+  employeurOption.style.display = "none";
+
 document.getElementById("inscription-dropdown").addEventListener("mouseover", function() {
-    interimaireOption.style.display = "block";
-    employeurOption.style.display = "block";
-  });
-  
+  interimaireOption.style.display = "block";
+  employeurOption.style.display = "block";
+});
+
 document.getElementById("inscription-dropdown").addEventListener("mouseout", function() {
-    interimaireOption.style.display = "none";
-    employeurOption.style.display = "none";
-  });
+  interimaireOption.style.display = "none";
+  employeurOption.style.display = "none";
+});
+
 });
 
 /* -------------------------------------------------------------------- */
